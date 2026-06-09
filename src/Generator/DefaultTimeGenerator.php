87de7b1e4f79ff5342f1e0ea2860e930a7abb68a
kun <gjing8865@gmail.com>
2026-06-09 10:22:59 +0000

    更新

--- src/Generator/DefaultTimeGenerator.php
+++ src/Generator/DefaultTimeGenerator.php
@@ -23,8 +23,7 @@ use Ramsey\Uuid\Provider\TimeProviderInterface;
 use Ramsey\Uuid\Type\Hexadecimal;
 use Throwable;
 
-use function ctype_xdigit;
-use function dechex;
+use function hash;
 use function hex2bin;
 use function is_int;
 use function pack;
@@ -43,8 +42,9 @@ class DefaultTimeGenerator implements TimeGeneratorInterface
 {
     private static string $clockSeqSeed = '';
     private static int $clockSeqIndex = 0;
+    private static int $clockSeqCounter = 0;
     /** @var int[] */
-    private static array $clockSeqPool = [];
+    private static array $clockSeqParts = [];
 
     public function __construct(
         private NodeProviderInterface $nodeProvider,
@@ -65,25 +65,44 @@ class DefaultTimeGenerator implements TimeGeneratorInterface
             $node = $node->toString();
         }
 
-        $node = $this->getValidNode($node);
+        if ($node === null) {
+            $node = $this->nodeProvider->getNode();
+        }
+
+        if (is_int($node)) {
+            $nodeBin = pack('n', ($node >> 32) & 0xffff) . pack('N', $node & 0xffffffff);
+        } else {
+            $nodeBin = hex2bin(str_pad((string) $node, 12, '0', STR_PAD_LEFT));
+            if ($nodeBin === false) {
+                throw new InvalidArgumentException('Invalid node value');
+            }
+        }
 
         if ($clockSeq === null) {
             if (self::$clockSeqIndex === 0) {
-                try {
-                    self::$clockSeqSeed = random_bytes(10);
-                    /** @var int[] $pool */
-                    $pool = unpack('n*', self::$clockSeqSeed);
-                    foreach ($pool as &$v) {
-                        $v &= 0x3fff;
+                if (self::$clockSeqSeed === '') {
+                    try {
+                        self::$clockSeqSeed = random_bytes(16);
+                    } catch (Throwable $exception) {
+                        throw new RandomSourceException($exception->getMessage(), (int) $exception->getCode(), $exception);
                     }
-                    unset($v);
-                    self::$clockSeqPool = $pool;
-                    self::$clockSeqIndex = 5;
-                } catch (Throwable $exception) {
-                    throw new RandomSourceException($exception->getMessage(), (int) $exception->getCode(), $exception);
+                } else {
+                    self::$clockSeqSeed = hash('sha512', self::$clockSeqSeed, true);
                 }
+
+                /** @var int[] $s */
+                $s = unpack('l*', self::$clockSeqSeed);
+                $s[] = ($s[1] >> 8 & 0xff0000) | ($s[2] >> 16 & 0xff00) | ($s[3] >> 24 & 0xff);
+                $s[] = ($s[4] >> 8 & 0xff0000) | ($s[5] >> 16 & 0xff00) | ($s[6] >> 24 & 0xff);
+                $s[] = ($s[7] >> 8 & 0xff0000) | ($s[8] >> 16 & 0xff00) | ($s[9] >> 24 & 0xff);
+                $s[] = ($s[10] >> 8 & 0xff0000) | ($s[11] >> 16 & 0xff00) | ($s[12] >> 24 & 0xff);
+                $s[] = ($s[13] >> 8 & 0xff0000) | ($s[14] >> 16 & 0xff00) | ($s[15] >> 24 & 0xff);
+
+                self::$clockSeqParts = $s;
+                self::$clockSeqIndex = 21;
             }
-            $clockSeq = self::$clockSeqPool[--self::$clockSeqIndex];
+
+            $clockSeq = (self::$clockSeqParts[--self::$clockSeqIndex] + self::$clockSeqCounter++) & 0x3fff;
         }
 
         $time = $this->timeProvider->getTime();
@@ -102,33 +121,5 @@ class DefaultTimeGenerator implements TimeGeneratorInterface
         $timeBytes = (string) hex2bin($timeHex);
         ['hi' => $hi, 'lo' => $lo] = unpack('Nhi/Nlo', $timeBytes);
 
-        return pack('Nnnn', $lo, ($hi >> 16) & 0xffff, $hi & 0xffff, $clockSeq) . $node;
+        return pack('Nnnn', $lo, ($hi >> 16) & 0xffff, $hi & 0xffff, $clockSeq) . $nodeBin;
     }
-
-    /**
-     * Uses the node provider given when constructing this instance to get the node ID (usually a MAC address)
-     *
-     * @param int | string | null $node A node value that may be used to override the node provider
-     *
-     * @return string 6-byte binary string representation of the node
-     *
-     * @throws InvalidArgumentException
-     */
-    private function getValidNode(int | string | null $node): string
-    {
-        if ($node === null) {
-            $node = $this->nodeProvider->getNode();
-        }
-
-        // Convert the node to hex if it is still an integer.
-        if (is_int($node)) {
-            $node = dechex($node);
-        }
-
-        if (!ctype_xdigit((string) $node) || strlen((string) $node) > 12) {
-            throw new InvalidArgumentException('Invalid node value');
-        }
-
-        return (string) hex2bin(str_pad((string) $node, 12, '0', STR_PAD_LEFT));
-    }
-}
