f3d22483a3655f9210103d29950240c92b977473
kun <gjing8865@gmail.com>
2026-06-09 09:48:57 +0000

    更新

--- src/Generator/DefaultTimeGenerator.php
+++ src/Generator/DefaultTimeGenerator.php
@@ -23,14 +23,16 @@ use Ramsey\Uuid\Provider\TimeProviderInterface;
 use Ramsey\Uuid\Type\Hexadecimal;
 use Throwable;
 
-use function dechex;
-use function hex2bin;
+use function hash;
+use function hexdec;
 use function is_int;
 use function pack;
-use function preg_match;
+use function random_bytes;
 use function sprintf;
 use function str_pad;
 use function strlen;
+use function strtolower;
+use function unpack;
 
 use const STR_PAD_LEFT;
 
@@ -39,6 +41,14 @@ use const STR_PAD_LEFT;
  */
 class DefaultTimeGenerator implements TimeGeneratorInterface
 {
+    private static ?string $seed = null;
+    private static int $seedIndex = 0;
+
+    /** @var int[] */
+    private static array $seedParts = [];
+
+    private static int $clockSeq = 0;
+
     public function __construct(
         private NodeProviderInterface $nodeProvider,
         private TimeConverterInterface $timeConverter,
@@ -48,7 +58,7 @@ class DefaultTimeGenerator implements TimeGeneratorInterface
 
     /**
      * @throws InvalidArgumentException if the parameters contain invalid values
-     * @throws RandomSourceException if random_int() throws an exception/error
+     * @throws RandomSourceException if random_bytes() throws an exception/error
      *
      * @inheritDoc
      */
@@ -56,17 +66,50 @@ class DefaultTimeGenerator implements TimeGeneratorInterface
     {
         if ($node instanceof Hexadecimal) {
             $node = $node->toString();
+        } elseif ($node === null) {
+            $node = $this->nodeProvider->getNode();
         }
 
-        $node = $this->getValidNode($node);
+        if (is_int($node)) {
+            $nodeBytes = pack('nN', ($node >> 32) & 0xffff, $node & 0xffffffff);
+        } else {
+            $nodeStr = (string) $node;
+            if (strlen($nodeStr) > 12) {
+                throw new InvalidArgumentException('Invalid node value');
+            }
+            $nodeInt = hexdec($nodeStr);
+            $nodeBytes = pack('nN', ($nodeInt >> 32) & 0xffff, $nodeInt & 0xffffffff);
+            if (unpack('H*', $nodeBytes)[1] !== str_pad(strtolower($nodeStr), 12, '0', STR_PAD_LEFT)) {
+                throw new InvalidArgumentException('Invalid node value');
+            }
+        }
 
         if ($clockSeq === null) {
-            try {
-                // This does not use "stable storage"; see RFC 9562, section 6.3.
-                $clockSeq = random_int(0, 0x3fff);
-            } catch (Throwable $exception) {
-                throw new RandomSourceException($exception->getMessage(), (int) $exception->getCode(), $exception);
+            if (self::$seedIndex === 0) {
+                if (self::$seed === null) {
+                    try {
+                        self::$seed = random_bytes(16);
+                    } catch (Throwable $exception) {
+                        throw new RandomSourceException($exception->getMessage(), (int) $exception->getCode(), $exception);
+                    }
+                    self::$clockSeq = unpack('n', self::$seed)[1] & 0x3fff;
+                }
+                self::$seed = hash('sha512', self::$seed, true);
+
+                /** @var int[] $s */
+                $s = unpack('l*', self::$seed);
+                $s[] = ($s[1] >> 8 & 0xff0000) | ($s[2] >> 16 & 0xff00) | ($s[3] >> 24 & 0xff);
+                $s[] = ($s[4] >> 8 & 0xff0000) | ($s[5] >> 16 & 0xff00) | ($s[6] >> 24 & 0xff);
+                $s[] = ($s[7] >> 8 & 0xff0000) | ($s[8] >> 16 & 0xff00) | ($s[9] >> 24 & 0xff);
+                $s[] = ($s[10] >> 8 & 0xff0000) | ($s[11] >> 16 & 0xff00) | ($s[12] >> 24 & 0xff);
+                $s[] = ($s[13] >> 8 & 0xff0000) | ($s[14] >> 16 & 0xff00) | ($s[15] >> 24 & 0xff);
+
+                self::$seedParts = $s;
+                self::$seedIndex = 21;
             }
+
+            self::$clockSeq = (self::$clockSeq + (self::$seedParts[self::$seedIndex--] & 0xffffff)) & 0x3fff;
+            $clockSeq = self::$clockSeq;
         }
 
         $time = $this->timeProvider->getTime();
@@ -76,43 +119,20 @@ class DefaultTimeGenerator implements TimeGeneratorInterface
             $time->getMicroseconds()->toString()
         );
 
-        $timeHex = str_pad($uuidTime->toString(), 16, '0', STR_PAD_LEFT);
+        $timeHex = $uuidTime->toString();
 
-        if (strlen($timeHex) !== 16) {
-            throw new TimeSourceException(sprintf('The generated time of \'%s\' is larger than expected', $timeHex));
+        if (strlen($timeHex) > 16) {
+            throw new TimeSourceException(sprintf('The generated time of \'%s\' is larger than expected', str_pad($timeHex, 16, '0', STR_PAD_LEFT)));
         }
 
-        $timeBytes = (string) hex2bin($timeHex);
+        $timeInt = hexdec($timeHex);
 
-        return $timeBytes[4] . $timeBytes[5] . $timeBytes[6] . $timeBytes[7]
-            . $timeBytes[2] . $timeBytes[3] . $timeBytes[0] . $timeBytes[1]
-            . pack('n*', $clockSeq) . $node;
-    }
-
-    /**
-     * Uses the node provider given when constructing this instance to get the node ID (usually a MAC address)
-     *
-     * @param int | string | null $node A node value that may be used to override the node provider
-     *
-     * @return string 6-byte binary string representation of the node
-     *
-     * @throws InvalidArgumentException
-     */
-    private function getValidNode(int | string | null $node): string
-    {
-        if ($node === null) {
-            $node = $this->nodeProvider->getNode();
-        }
-
-        // Convert the node to hex if it is still an integer.
-        if (is_int($node)) {
-            $node = dechex($node);
-        }
-
-        if (!preg_match('/^[A-Fa-f0-9]+$/', (string) $node) || strlen((string) $node) > 12) {
-            throw new InvalidArgumentException('Invalid node value');
-        }
-
-        return (string) hex2bin(str_pad((string) $node, 12, '0', STR_PAD_LEFT));
+        return pack(
+            'Nnnn',
+            $timeInt & 0xffffffff,
+            ($timeInt >> 32) & 0xffff,
+            ($timeInt >> 48) & 0xffff,
+            $clockSeq
+        ) . $nodeBytes;
     }
 }
